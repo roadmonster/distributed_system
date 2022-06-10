ebb57988cd53bb24526fb351f5afee48275413fb
Hao_Li <roadmonster1989@gmail.com>
2022-06-10 16:23:24 -0700

    done testpublisher class and forex prover class

--- CurrentAbitrageDetection/broadcaster.py
+++ CurrentAbitrageDetection/broadcaster.py
@@ -1,6 +1,7 @@
 
 from datetime import datetime, timedelta
 import random
+import selectors
 import socket
 
 
@@ -55,3 +56,59 @@ class TestPublisher(object):
                 xxx, yyy = sorted(random.sample(list(self.reference), 2))
                 xxx_per_usd = self.reference[xxx] if xxx not in REVERSE_QUOTED else 1/self.reference[xxx]
                 yyy_per_usd = self.reference[yyy] if yyy not in REVERSE_QUOTED else 1/self.reference[yyy]
+                rate = (yyy_per_usd / xxx_per_usd) * random.gauss(1.0, 0.01)
+                if random.random()< 0.5:
+                    print('putting n a 3 way cycle')
+                    quotes.append({
+                        'cross': '{}/{}'.format(xxx, yyy), 
+                        'price': rate
+                    })
+                else:
+                    print('putting a 4-way cycle')
+                    quotes.append({
+                        'cross':'{}/CAD'.format(xxx),
+                        'price': rate/2
+                    })
+
+                    quotes.append({
+                        'cross':'CAD/{}'.format(yyy),
+                        'price': rate*2
+                    })
+            message = prov_utils.marshal_message(quotes)
+            for subscriber in self.subscriptions:
+                print('publishing {} to {}'.format(quotes, subscriber))
+                self.socket.sendto(message, subscriber)
+            
+            return 1.0
+
+class ForexProvider(object):
+    
+    def __init__(self, request_addr, pub_class) -> None:
+        self.selector = selectors.DefaultSelector()
+        self.subscription_req= self.start_server(request_addr)
+        self.selector.register(self.subscription_req, selectors.EVENT_READ)
+        self.publisher = pub_class()
+    
+    def run_forever(self):
+        print('waiting for subscribers on {}'.format(self.subscription_req))
+        next_timeout = 0.2
+        while True:
+            events = self.selector.select(next_timeout)
+            for k, m in events:
+                self.register_subscription()
+        
+    def register_subscription(self):
+        data, addr = self.subscription_req.recvfrom(REQUEST_SIZE)
+        subscriber = prov_utils.deserialize(data)
+        self.publisher.register_subs(subscriber)
+
+    @staticmethod
+    def start_server(addr):
+        listener = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        listener.bind(addr)
+        listener.settimeout(0.2)
+        return listener
+
+if __name__ == '__main__':
+    fxp = ForexProvider(REQUEST_ADDRESS, TestPublisher)
+    fxp.run_forever()
